6b447bff1c1c0431a286141619a8c38ee77ba387
Eric Medvedev <erson.99.cool@gmail.com>
2020-10-20 18:08:11 +0300

    Adding Angular Material in project

---
+++ src/app/ui.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatStepperModule } from '@angular/material/stepper';
+
+const material= [
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatListModule,
+    MatCardModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatChipsModule,
+    MatCheckboxModule, 
+    MatIconModule, 
+    MatExpansionModule,
+    MatStepperModule,
+]
+
+@NgModule({
+    imports: [material],
+    exports: [material]
+  })
+  export class UiModule { }
+  
