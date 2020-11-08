f5b3db4a041f9425994a2a91059df86692a73c69
Eric Medvedev <erson.99.cool@gmail.com>
2020-11-08 23:54:11 +0300

    Basics implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared';
 import { AuthModule } from './auth/auth.module';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
