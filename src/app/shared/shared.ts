f515d5a021d58cbeae54178998e27a493c766509
Eric Medvedev <erson.99.cool@gmail.com>
2020-11-09 02:49:16 +0300

    Remove unused imports

--- src/app/shared/shared.ts
+++ src/app/shared/shared.ts
@@ -1,8 +1,4 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { UiSharedModule } from './ui-shared.module';
 
 @NgModule({
